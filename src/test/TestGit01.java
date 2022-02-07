0a28b23c9340636f53ff54df405258571ec78851
huaidanmu <1301218756@qq.com>
2022-02-07 13:51:07 +0800

    第二次修改

--- src/test/TestGit01.java
+++ src/test/TestGit01.java
@@ -5,5 +5,6 @@ public class TestGit01 {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("第一次修改");
+        System.out.println("第二次修改");
     }
 }
